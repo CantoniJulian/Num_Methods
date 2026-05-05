fc1a5450c9995b1c987a1a5fecde3047a02fb1d4
Julián <julianeliascantoni@gmail.com>
2026-05-04 23:09:01 -0300

    Fifth commit. Finally added Gauss-Seidel method for Equation Systems. TODO: Fix critical derivative value condition for FP method. Add LU method. Add CSV parsing function (not main priority).

--- Sistemas-Ecuaciones/lib.h
+++ Sistemas-Ecuaciones/lib.h
@@ -1,4 +1,4 @@
-#include <ctime>
+#include <cmath>
 #include <random>
 #include <iostream>
 
@@ -20,6 +20,13 @@ class Matrix
         void setElement(int i, int j, double value);
 
         void print_matrix();
+
+        void swap_rows(int a, int b); 
+
+        int getSize()
+        {
+            return size_m;
+        }
         
 
         
@@ -29,7 +36,7 @@ class Method
 {
     private:
         int m_size;
-        Matrix *M ;
+        Matrix *M;
         int it;
         double *solution;
 
@@ -45,7 +52,21 @@ class Method
                 solution[i] = 0;
             }
 
-        };
+        }
+
+        Method(Matrix *A)
+        {
+            M = A;
+            m_size = M->getSize();
+            solution = new double[m_size];
+
+            
+            for (int i = 0; i < m_size; i++)
+            {
+                solution[i] = 0;
+            }
+
+        }
 
         virtual void Solve()
         {};
@@ -85,7 +106,7 @@ class Method
                 sum += solution[i]*getM()->getElement(0, i);
             }  
             std:: cout << "La sumita dio (funciona, pls): "<< sum;
-        }
+        }    
         
 };
 
@@ -117,8 +138,63 @@ class GaussianElimination : public Method
 
 };
 
+class Gauss_Seidel : public Method
+{
+    private:
+        double *errors;
+        double *sol_prev;
+        int *dominant_var;
+    public:
+     Gauss_Seidel() 
+        {
+            errors = new double[get_m_size()];
+            sol_prev = new double[get_m_size()];
+            dominant_var = new int[get_m_size()];
+            for (int i = 0; i<get_m_size(); i++)
+            {
+                sol_prev[i] = 0;
+                errors[i] = 0;
+                dominant_var[i] = -1;
+            }
+            std:: cout << "dom iniciado en: "<< dominant_var[0] << std:: endl;
+           
+        }
 
+        Gauss_Seidel(Matrix *A) : Method(A)
+        {
+            errors = new double[get_m_size()];
+            sol_prev = new double[get_m_size()];
+            dominant_var = new int[get_m_size()];
+            for (int i = 0; i<get_m_size(); i++)
+            {
+                sol_prev[i] = 0;
+                errors[i] = 0;
+                dominant_var[i] = -1;
+            }
+        }
+        void Iterate_solutions();
+        void Iterate_Error();
+        double get_sum_i(int i);
+        bool Is_Diagonally_Dom();
 
+        void Solve() override
+        {
+             std:: cout << "dom iniciado en: "<< dominant_var[0] << std:: endl;
+
+            if (Is_Diagonally_Dom())
+            {
+                Iterate_Error();
+                print_solutions();
+                EpiCheck();
+            }
+            else
+            {
+                std:: cout << "Matriz no diagonalmente dominante. Gauss-Seidel no sirve." << std :: endl;
+            }
+        }
+   
+
+};
 
 // implementacion de Matriz :3
 Matrix :: Matrix(int n) : size_m(n)
@@ -155,12 +231,30 @@ void Matrix :: print_matrix()
 
 double Matrix :: getElement(int i, int j)
 {
-    return mtrx[i][j];
+    if (i <= size_m && j <= size_m)
+    {
+        return mtrx[i][j];
+    }
+    else
+    {
+        throw 400;
+        
+    }
+    return 0;
 }
 
 void Matrix :: setElement(int i, int j, double value)
 {
     mtrx[i][j] = value;
+}
+
+void Matrix :: swap_rows(int a, int b)
+{
+    double *aux = mtrx[a];
+    mtrx[a] = mtrx[b];
+    mtrx[b] = aux;
+    
+    // delete aux; No seas gilun y no borres el aux que es un puntero que apunta a a algo importante.
 }
 
 
@@ -182,9 +276,6 @@ bool GaussianElimination :: ForwardElimination() //return false si encuentra fil
 
             }
         }
-
-
-
     }
 
 
@@ -226,10 +317,126 @@ void GaussianElimination :: BackwwardsSubstitution()
             sum += getM()->getElement(i, j) * get_sol(j);
             
         }
-        std:: cout << sum << std:: endl;
+        std:: cout << "oal"<< sum << std:: endl;
         
         x_i = (getM()->getElement(i, get_m_size()) - sum) / (getM()->getElement(i, i));
         set_solution(x_i, i);
     }
+}
+
+void Gauss_Seidel :: Iterate_solutions()
+{
+    for (int i = 0; i<get_m_size(); i++)
+    {
+        double b_i = 0;
+        
+        b_i = getM()->getElement(i, get_m_size());
+
+        
+
+        double new_solution = (b_i - get_sum_i(i)) / getM()->getElement(i, i);
+
+        set_solution(new_solution, i);
+    }
+}
+
+double Gauss_Seidel :: get_sum_i(int i)
+{
+    double sum = 0;
+    for (int j = 0; j < get_m_size(); j++) // NO PONGAS -1 EN LOS FOR porque la condición dice meno, pajero del orto
+    {
+        if (j != i)
+        {
+            sum += getM()->getElement(i, j) * get_sol(j);
+        }
+        
+        std :: cout << "sum: " << sum; 
+    }
+    return sum;
+}
+
+void Gauss_Seidel :: Iterate_Error()
+{
+    int it_count = 0;
+/*     for (int i = 0; i < get_m_size(); i++)
+    {
+        errors[i] = get_sol(i) - sol_prev[i];
+        Iterate_solutions();
+        while (errors[i] > 0.0001 && it_count < 50)
+        {
+            std :: cout << get_sol(i) << std :: endl;
+            errors[i] = get_sol(i) - sol_prev[i];
+            Iterate_solutions();
+            it_count ++;
+        }
+
+    }    */
+
+    bool converged = false;
+        while (!converged && it_count < 500)
+        {
+            Iterate_solutions();
+            converged = true;
+
+            for (int i = 0; i < get_m_size(); i++)
+            {
+                errors[i] = std::fabs(get_sol(i) - sol_prev[i]);
+                if (errors[i] > 0.0000001)
+                {
+                    converged = false;
+                }
+                sol_prev[i] = get_sol(i);
+            }
+            it_count ++;
+        }
+        std :: cout << "It_count: "<< it_count << std :: endl;
+}
+
+bool Gauss_Seidel :: Is_Diagonally_Dom()
+{
+    
+    for (int i = 0; i<get_m_size(); i++)
+    {
+
+        for (int j = 0; j<get_m_size(); j++)
+        {
+            double sum = 0;
+            for (int k = 0; k<get_m_size(); k++)
+            {
+                if (k != j)
+                {
+                    sum += abs(getM()->getElement(i,k));
+                }
+                
+            }
+            
+            if (abs(getM()->getElement(i,j)) > abs(sum))
+            {
+                if (dominant_var[j] == -1)
+                {
+                    dominant_var[j] = i;
+                    std:: cout << "Dominante en fila"<< i << " es la columna : " << j << std :: endl;
+                    getM()->swap_rows(j, i);
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if (dominant_var[i] == -1)
+        {
+            return false;
+        }
+
+    }
+
+    std :: cout << "Bueno..." << std :: endl;
+    for (int a = 0; a<get_m_size(); a++)
+    {
+        getM()->swap_rows(a, dominant_var[a]);
+    }
+    return true;
 }
 
--- Sistemas-Ecuaciones/main.cpp
+++ Sistemas-Ecuaciones/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    GaussianElimination G;
+/*     GaussianElimination G;
     G.getM()-> print_matrix();
 
 
@@ -13,6 +13,51 @@ int main()
     std::cout << G.ForwardElimination() << std::endl;
     G.getM()->print_matrix();
 
-    G.Solve();
+    G.Solve(); */
     
+    double puto [12] = {10, 2, -1, 15, -3, 8, 2, 11, 1, 4, 15,-6};
+    Matrix *A = new Matrix(3);
+/* 
+    for (int i = 0; i < A->getSize(); i++)
+    {
+        for (int j = 0; j < A->getSize()+1; j++)
+        {
+            double in;
+            std :: cout << "Elemento " << i << ", " << j;
+            std :: cin >> in;
+            A->setElement(i, j, in); 
+            std :: cout << std :: endl;
+        }
+        
+    } */
+   
+    A->setElement(0, 0, puto[0]);
+    A->setElement(0, 1, puto[1]);
+    A->setElement(0, 2, puto[2]);
+    A->setElement(0, 3, puto[3]);
+    A->setElement(1, 0, puto[4]);
+    A->setElement(1, 1, puto[5]);
+    A->setElement(1, 2, puto[6]);
+    A->setElement(1, 3, puto[7]);
+    A->setElement(2, 0, puto[8]);
+    A->setElement(2, 1, puto[9]);
+    A->setElement(2, 2, puto[10]);
+    A->setElement(2, 3, puto[11]);
+
+    std ::  cout << std :: endl;
+    A->print_matrix();
+
+    
+
+    Gauss_Seidel G2 = Gauss_Seidel(A);
+
+    std :: cout << std :: endl;
+    G2.Solve();
+
+    std :: cout << std :: endl;
+    G2.getM()->print_matrix();
+    std:: cout << std:: endl;
+    
+    //std:: cout << G2.Is_Diagonally_Dom();
+   
 }
